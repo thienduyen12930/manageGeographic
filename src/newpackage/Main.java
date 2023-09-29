ce19393479e5fec700a232571f393b14ea055674
thienduyen12930 <thienduyen1209@gmail.com>
2023-09-29 14:28:30 +0700

    manegeGeographic

---
+++ src/newpackage/Main.java
@@ -0,0 +1,12 @@
+
+import controller.ManageEastAsiaCountries;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        ManageEastAsiaCountries controller = new ManageEastAsiaCountries();
+        controller.run();
+    }
+}
